1688bff804b8232fc12134d7f7f83f758a380866
Thuan <you@example.com>
2023-03-23 21:11:08 -0500

    Base code

--- imageEncryption.py
+++ imageEncryption.py
@@ -1,1 +1,24 @@
-print("Hello World")
+try:
+    key = 21
+
+    path = "tree.jpg"
+     
+    fin = open(path, 'rb')
+     
+    image = fin.read()
+    fin.close()
+     
+    image = bytearray(image)
+ 
+    for index, values in enumerate(image):
+        image[index] = values ^ key
+ 
+    fin = open(path, 'wb')
+
+    fin.write(image)
+    fin.close()
+    print('Encryption Done...')
+ 
+     
+except Exception:
+    print('Error caught : ', Exception.__name__)
