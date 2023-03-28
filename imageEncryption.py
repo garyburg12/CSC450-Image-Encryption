cb95ec3249023dbbc743c4c4d66ff20bf183f10b
Thuan <you@example.com>
2023-03-28 16:42:52 -0500

    updates

--- imageEncryption.py
+++ imageEncryption.py
@@ -1,24 +1,27 @@
-try:
-    key = 21
-
-    path = "tree.jpg"
-     
-    fin = open(path, 'rb')
-     
-    image = fin.read()
-    fin.close()
-     
-    image = bytearray(image)
- 
-    for index, values in enumerate(image):
-        image[index] = values ^ key
- 
-    fin = open(path, 'wb')
-
-    fin.write(image)
-    fin.close()
-    print('Encryption Done...')
- 
-     
-except Exception:
-    print('Error caught : ', Exception.__name__)
+from PIL import Image
+# Import an image from directory:
+image = Image.open("test.png")
+  
+# Extracting pixel map:
+pixel_map = image.load()
+  
+# Extracting the width and height 
+# of the image:
+width, height = image.size
+  
+# taking half of the width:
+for i in range(width):
+    for j in range(height):
+        
+        # getting the RGB pixel value.
+        r, g, b, p = image.getpixel((i, j))
+          
+        # Apply formula of grayscale:
+        grayscale = (0.5*r + 0.587*g + 0.5*b)
+  
+        # setting the pixel value.
+        pixel_map[i, j] = (int(grayscale), int(grayscale), int(grayscale))
+  
+# Saving the final output
+# as "grayscale.png":
+image.save("grayscale.png")
