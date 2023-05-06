314d727b585772a1c3bc740fe84aeb20e890a715
BonyHeight <casmith322@gmail.com>
2023-05-06 13:04:24 -0500

    nothing important, added some convenience to dummy

--- backend/dummyprogram.py
+++ backend/dummyprogram.py
@@ -1,5 +1,5 @@
 from PIL import Image
-x = Image.open("circle.png")
+x = Image.open("inversecircle.png")
 
 pixel_map = x.load()
 
@@ -8,6 +8,8 @@ for i in range (4):
         a, b, c = x.getpixel((i, j))
         #if abs((i-2)*(j-2)) == 1:
         if 0 < i and i < 3 and 0 < j and j < 3:
-            pixel_map[i, j] = (255, 255, 255)
+            #pixel_map[i, j] = (255, 255, 255)
+            pixel_map[i, j] = (0, 0, 0)
         #pixel_map[i, j] = (0, 0, 0)
-x.save("circle.png")
+        #pixel_map[i, j] = (255, 255, 255)
+x.save("inversecircle.png")
